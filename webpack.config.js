b72cfcb3891b68e958b5fd7e980f17d01306ae9b
Daniel Velazquez <danip293@gmail.com>
2024-10-24 13:18:43 -0600

    mini css extract plugin basic implementation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -7,11 +8,20 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       scriptLoading: 'blocking',
       inject: 'head',
       template: path.resolve(__dirname, 'public', 'index.html'),
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
